298addaa2d6dadf2dd854f7aec81d06acc982799
Ilya_Pryzhykau <tuarone@gmail.com>
2024-05-11 20:34:44 +0200

    5

--- blogicum/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class About(TemplateView):
-    template_name = 'pages/about.html'
-
-
-class Rules(TemplateView):
-    template_name = 'pages/rules.html'
