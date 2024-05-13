053da74928db1980f4d4ef55aad28e984ea6eba4
Ilya_Pryzhykau <tuarone@gmail.com>
2024-05-13 22:57:11 +0200

    17

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -13,16 +13,32 @@ class RulesPageView(TemplateView):
 
 
 def page_not_found(request, *args, template_name='pages/404.html', **kwargs):
-    return render(request, template_name, status=HTTPStatus.NOT_FOUND)
+    return render(
+        request,
+        template_name,
+        status=HTTPStatus.NOT_FOUND
+    )
 
 
 def server_error(request, *args, template_name='pages/500.html', **kwargs):
-    return render(request, template_name, status=HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(
+        request,
+        template_name,
+        status=HTTPStatus.INTERNAL_SERVER_ERROR
+    )
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=HTTPStatus.FORBIDDEN)
+    return render(
+        request,
+        'pages/403csrf.html',
+        status=HTTPStatus.FORBIDDEN
+    )
 
 
 def permission_denied(request, exception):
-    return render(request, 'pages/403.html', status=HTTPStatus.FORBIDDEN)
+    return render(
+        request,
+        'pages/403.html',
+        status=HTTPStatus.FORBIDDEN
+    )
